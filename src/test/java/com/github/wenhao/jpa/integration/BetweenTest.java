c41a5af94f57786692105339e61ddb1bcb6fad75
Hao Wen <wenhao@126.com>
2016-10-12 13:15:08 +0800

    [Wen Hao] - add between integration test.

---
+++ src/test/java/com/github/wenhao/jpa/integration/BetweenTest.java
@@ -0,0 +1,59 @@
+package com.github.wenhao.jpa.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.wenhao.jpa.Specifications;
+import com.github.wenhao.jpa.builder.PersonBuilder;
+import com.github.wenhao.jpa.model.Person;
+import com.github.wenhao.jpa.repository.PersonRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Range;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class BetweenTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    public void should_be_able_to_find_by_using_between() throws ParseException {
+        // given
+        Person jack = new PersonBuilder()
+            .name("Jack")
+            .birthday(getDate("1987-11-14"))
+            .build();
+        Person eric = new PersonBuilder()
+            .name("Eric")
+            .birthday(getDate("1990-10-12"))
+            .build();
+        personRepository.save(jack);
+        personRepository.save(eric);
+
+        // when
+        Specification<Person> specification = new Specifications<Person>()
+            .between(Objects.nonNull(jack.getBirthday()), "birthday", new Range<>(getDate("1980-01-01"), getDate
+                ("1989-12-31")))
+            .build();
+
+        List<Person> persons = personRepository.findAll(specification);
+
+        // then
+        assertThat(persons.size()).isEqualTo(1);
+    }
+
+    private Date getDate(String source) throws ParseException {
+        return new SimpleDateFormat("yyyy-MM-dd").parse(source);
+    }
+}
