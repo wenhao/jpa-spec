5acf86e21f96f2b7645745775a8e100d78930539
Hao Wen <wenhao@126.com>
2016-10-12 13:22:45 +0800

    [Wen Hao] - add numerical integration test.

---
+++ src/test/java/com/github/wenhao/jpa/integration/GreatThanTest.java
@@ -0,0 +1,49 @@
+package com.github.wenhao.jpa.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.wenhao.jpa.Specifications;
+import com.github.wenhao.jpa.builder.PersonBuilder;
+import com.github.wenhao.jpa.model.Person;
+import com.github.wenhao.jpa.repository.PersonRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class GreatThanTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    public void should_be_able_to_find_by_using_great_than() {
+        // given
+        Person jack = new PersonBuilder()
+            .name("Jack")
+            .age(20)
+            .build();
+        Person eric = new PersonBuilder()
+            .name("Eric")
+            .age(18)
+            .build();
+        personRepository.save(jack);
+        personRepository.save(eric);
+
+        // when
+        Specification<Person> specification = new Specifications<Person>()
+            .gt("age", 18)
+            .build();
+
+        List<Person> persons = personRepository.findAll(specification);
+
+        // then
+        assertThat(persons.size()).isEqualTo(1);
+    }
+}
