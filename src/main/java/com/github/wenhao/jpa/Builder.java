ad074b74a64aa0fd38b7ba753fe49c73c5f78e4b
Marcin Soja <dev@marcinsoja.pl>
2017-03-01 20:51:57 +0100

    Remove duplicated 'if (condition)'

--- src/main/java/com/github/wenhao/jpa/Builder.java
+++ src/main/java/com/github/wenhao/jpa/Builder.java
@@ -39,10 +39,7 @@ public class Builder<T> {
     }
 
     public Builder<T> eq(boolean condition, String property, Object... values) {
-        if (condition) {
-            this.specifications.add(new EqualSpecification<T>(property, values));
-        }
-        return this;
+        return this.predicate(condition, new EqualSpecification<T>(property, values));
     }
 
     public Builder<T> ne(String property, Object... values) {
@@ -50,10 +47,7 @@ public class Builder<T> {
     }
 
     public Builder<T> ne(boolean condition, String property, Object... values) {
-        if (condition) {
-            this.specifications.add(new NotEqualSpecification<T>(property, values));
-        }
-        return this;
+        return this.predicate(condition, new NotEqualSpecification<T>(property, values));
     }
 
     public Builder<T> gt(String property, Number number) {
@@ -61,10 +55,7 @@ public class Builder<T> {
     }
 
     public Builder<T> gt(boolean condition, String property, Number number) {
-        if (condition) {
-            this.specifications.add(new GtSpecification<T>(property, number));
-        }
-        return this;
+        return this.predicate(condition, new GtSpecification<T>(property, number));
     }
 
     public Builder<T> ge(String property, Number number) {
@@ -72,10 +63,7 @@ public class Builder<T> {
     }
 
     public Builder<T> ge(boolean condition, String property, Number number) {
-        if (condition) {
-            this.specifications.add(new GeSpecification<T>(property, number));
-        }
-        return this;
+        return this.predicate(condition, new GeSpecification<T>(property, number));
     }
 
     public Builder<T> lt(String property, Number number) {
@@ -83,10 +71,7 @@ public class Builder<T> {
     }
 
     public Builder<T> lt(boolean condition, String property, Number number) {
-        if (condition) {
-            this.specifications.add(new LtSpecification<T>(property, number));
-        }
-        return this;
+        return this.predicate(condition, new LtSpecification<T>(property, number));
     }
 
     public Builder<T> le(String property, Number number) {
@@ -94,10 +79,7 @@ public class Builder<T> {
     }
 
     public Builder<T> le(boolean condition, String property, Number number) {
-        if (condition) {
-            this.specifications.add(new LeSpecification<T>(property, number));
-        }
-        return this;
+        return this.predicate(condition, new LeSpecification<T>(property, number));
     }
 
     public Builder<T> between(String property, Range<? extends Comparable<?>> range) {
@@ -105,10 +87,7 @@ public class Builder<T> {
     }
 
     public Builder<T> between(boolean condition, String property, Range<? extends Comparable<?>> range) {
-        if (condition) {
-            this.specifications.add(new BetweenSpecification<T>(property, range));
-        }
-        return this;
+        return this.predicate(condition, new BetweenSpecification<T>(property, range));
     }
 
     public Builder<T> like(String property, String... patterns) {
@@ -116,10 +95,7 @@ public class Builder<T> {
     }
 
     public Builder<T> like(boolean condition, String property, String... patterns) {
-        if (condition) {
-            this.specifications.add(new LikeSpecification<T>(property, patterns));
-        }
-        return this;
+        return this.predicate(condition, new LikeSpecification<T>(property, patterns));
     }
 
     public Builder<T> notLike(String property, String... patterns) {
@@ -127,10 +103,7 @@ public class Builder<T> {
     }
 
     public Builder<T> notLike(boolean condition, String property, String... patterns) {
-        if (condition) {
-            this.specifications.add(new NotLikeSpecification<T>(property, patterns));
-        }
-        return this;
+        return this.predicate(condition, new NotLikeSpecification<T>(property, patterns));
     }
 
     public Builder<T> in(String property, Object... values) {
@@ -138,10 +111,7 @@ public class Builder<T> {
     }
 
     public Builder<T> in(boolean condition, String property, Object... values) {
-        if (condition) {
-            this.specifications.add(new InSpecification<T>(property, values));
-        }
-        return this;
+        return this.predicate(condition, new InSpecification<T>(property, values));
     }
 
     public Builder<T> notIn(String property, Object... values) {
@@ -149,10 +119,7 @@ public class Builder<T> {
     }
 
     public Builder<T> notIn(boolean condition, String property, Object... values) {
-        if (condition) {
-            this.specifications.add(new NotInSpecification<T>(property, values));
-        }
-        return this;
+        return this.predicate(condition, new NotInSpecification<T>(property, values));
     }
 
     public Builder<T> predicate(Specification specification) {
